5d7ecbcef8e5021040b5f99b332ec71fb8acaa64
Muhammad Fakhrudin <muhfakhrudin04@gmail.com>
2025-11-19 23:57:17 +0700

    membuat kerangka halaman

---
+++ lib/pages/edit_rental_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditRentalPage extends StatelessWidget {
+  const EditRentalPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/form_rental_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FormRentalPage extends StatelessWidget {
+  const FormRentalPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
